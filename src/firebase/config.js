3ef12e3863dcf2ad9841abc7a5bc755d9bef85d9
Gabriel Schemoul <gabschemoul@gmail.com>
2023-03-07 17:55:16 +0100

    Added the new firebase Leads to the config + managed to submit new sign ups to firebase/airtable

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -12,10 +12,92 @@ const firebaseConfig = {
   measurementId: "G-8Z4LDT58XT",
 };
 
+// Your web app's Firebase configuration
+const firebaseLeadsConfig = {
+  apiKey: "AIzaSyBnL75mGrXv_FR1MZVFkdmOeP5isTn1tE0",
+  authDomain: "publicly-leads.firebaseapp.com",
+  projectId: "publicly-leads",
+  storageBucket: "publicly-leads.appspot.com",
+  messagingSenderId: "678315838423",
+  appId: "1:678315838423:web:3a8b1098db789d20ca9992",
+};
+
+let app;
+let appLeads;
+
+if (getApps().length === 0) {
+  app = initializeApp({
+    ...firebaseConfig,
+    projectId: firebaseConfig?.projectId,
+  });
+  appLeads = initializeApp(
+    {
+      ...firebaseLeadsConfig,
+      projectId: firebaseLeadsConfig?.projectId,
+    },
+    "leads"
+  );
+} else {
+  if (!getApp("leads")) {
+    appLeads = initializeApp(
+      {
+        ...firebaseLeadsConfig,
+        projectId: firebaseLeadsConfig?.projectId,
+      },
+      "leads"
+    );
+    app = getApp("[DEFAULT]");
+  }
+  if (!getApp("[DEFAULT]")) {
+    app = initializeApp({
+      ...firebaseConfig,
+      projectId: firebaseConfig?.projectId,
+    });
+
+    appLeads = getApp("leads");
+  }
+}
+
+const db = getFirestore(app);
+const dbLeads = getFirestore(appLeads);
+
+console.log(db);
+console.log(dbLeads);
+
+/*
 const app =
   getApps().length === 0
     ? initializeApp({ ...firebaseConfig, projectId: firebaseConfig?.projectId })
     : getApp();
 const db = getFirestore(app);
 
-export { app, db };
+
+const appLeads =
+  getApps().length === 0
+    ? initializeApp({
+        ...firebaseLeadsConfig,
+        projectId: firebaseLeadsConfig?.projectId,
+      })
+    : getApp();
+const dbLeads = getFirestore(appLeads);
+*/
+/*
+const app = initializeApp({
+  ...firebaseConfig,
+  projectId: firebaseConfig?.projectId,
+});
+const db = getFirestore(app);
+const appLeads = initializeApp({
+  ...firebaseLeadsConfig,
+  projectId: firebaseLeadsConfig?.projectId,
+});
+const dbLeads = getFirestore(appLeads);
+*/
+
+/*
+console.log(getApps.find((item) => item.name_ === firebaseConfig?.projectId));
+console.log(
+  getApps.find((item) => item.name_ === firebaseLeadsConfig?.projectId)
+);*/
+
+export { app, db, appLeads, dbLeads };
