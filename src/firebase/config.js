95373ab87048b2d22a26bd1fb5d41feb81a4e643
Gabriel Schemoul <gabschemoul@gmail.com>
2023-04-03 15:15:45 +0200

    Fixing beta user signing up 6

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -23,26 +23,6 @@ const firebaseLeadsConfig = {
   appId: process.env.FIREBASE_LEADS_APP_ID,
 };
 
-/*const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
-
-// Your web app's Firebase configuration
-const firebaseLeadsConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_LEADS_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_LEADS_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_LEADS_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_LEADS_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_LEADS_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_LEADS_APP_ID,
-};*/
-
 let app;
 let appLeads;
 
